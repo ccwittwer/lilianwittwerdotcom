cc17258327743ca135921d3bce909d4feaeeeb74
Clay Wittwer <ccwittwer@users.noreply.github.com>
2025-06-15 14:31:31 -0500

    Add console log

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,5 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+	console.log('redirecting to /profile');
 	throw redirect(307, '/profile');
 };
