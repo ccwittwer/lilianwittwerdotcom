fee485473fa4f7cbcebc1034e9f000bfb8ff4cd3
Clay Wittwer <ccwittwer@users.noreply.github.com>
2025-06-15 14:34:58 -0500

    remove the layout redirect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
 	import '../app.css';
 
-	onMount(() => {
-		// Redirect to default color scheme if at root
-		if (window.location.pathname === '/') {
-			goto('/profile');
-		}
-	});
+	// onMount(() => {
+	// 	// Redirect to default color scheme if at root
+	// 	if (window.location.pathname === '/') {
+	// 		goto('/profile');
+	// 	}
+	// });
 </script>
 
 <slot />
